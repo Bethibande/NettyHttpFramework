d24a803016f9b5360b7171b4ffafa1f7e4026521
Max <bethibande@gmail.com>
2023-07-30 09:57:47 +0200

    Path builder, optional path #29

--- base/src/main/kotlin/com/bethibande/http/routes/RouteBuilder.kt
+++ base/src/main/kotlin/com/bethibande/http/routes/RouteBuilder.kt
@@ -17,36 +17,72 @@ class RouteBuilder(
 
     private fun contactPath(path: String): String = (this.path + path).replace(PATH_REPLACE_REGEX, "/")
 
+    fun get(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.GET, handler))
+    }
+
     fun get(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.GET, handler))
+    }
+
+    fun post(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.POST, handler))
     }
 
     fun post(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.POST, handler))
     }
 
+    fun delete(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.DELETE, handler))
+    }
+
     fun delete(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.DELETE, handler))
+    }
+
+    fun patch(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.PATCH, handler))
     }
 
     fun patch(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.PATCH, handler))
     }
 
+    fun put(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.PUT, handler))
+    }
+
     fun put(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.PUT, handler))
+    }
+
+    fun head(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.HEAD, handler))
     }
 
     fun head(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.HEAD, handler))
     }
 
+    fun options(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.OPTIONS, handler))
+    }
+
     fun options(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.OPTIONS, handler))
     }
 
+    fun connect(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.CONNECT, handler))
+    }
+
     fun connect(path: String, handler: ResponseHook) {
         this.registry.register(Route(this.contactPath(path), HttpMethod.CONNECT, handler))
+    }
+
+    fun trace(handler: ResponseHook) {
+        this.registry.register(Route(this.path, HttpMethod.TRACE, handler))
     }
 
     fun trace(path: String, handler: ResponseHook) {
